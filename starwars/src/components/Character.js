847af17febd401b3d4682c5c1cd64ca2adc4873f
boahs <39522778+boahs@users.noreply.github.com>
2020-08-14 12:55:17 -0400

    styled

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,51 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Character = (props) => {
+  //   const StyledChar = styled.div`
+  //     font-size: ${(props) => props.theme.smallFont};
+  //   `;
+  //   const NameColor = styled.p`
+  //     color: ${(props) => props.theme.nameFont};
+  //   `;
+
+  const StyledChar = styled.div`
+    font-size: 1rem;
+    border: 6px solid black;
+    border-style: dashed;
+    border-radius: 10px;
+    margin-bottom: 10px;
+  `;
+
+  const NameFont = styled.p`
+    color: #d822e1;
+  `;
+
+  const HeightMassFont = styled.p`
+    color: #5f22e1;
+  `;
+
+  const SkinEyeColor = styled.p`
+    color: #47687b;
+  `;
+
+  const YearGenderColor = styled.p`
+    color: #2276e1;
+  `;
+
+  return (
+    <StyledChar>
+      <NameFont>Name: {props.name}</NameFont>
+      <HeightMassFont>Height: {props.height}</HeightMassFont>
+      <HeightMassFont>Mass: {props.mass}</HeightMassFont>
+      <SkinEyeColor>Hair Color: {props.hairColor}</SkinEyeColor>
+      <SkinEyeColor>Skin Color: {props.skinColor}</SkinEyeColor>
+      <SkinEyeColor>Eye Color: {props.eyeColor}</SkinEyeColor>
+      <YearGenderColor>Year Born: {props.birthYear}</YearGenderColor>
+      <YearGenderColor>Gender: {props.gender}</YearGenderColor>
+    </StyledChar>
+  );
+};
+
+export default Character;
---
+++ starwars/src/theme/theme.js
@@ -0,0 +1,9 @@
+const theme = {
+  smallFont: "0.1rem",
+  nameFont: "#D822E1",
+  heightMassFont: "#5F22E1",
+  skinEyeColor: "#22E1AE",
+  yearGenderHome: "#2276E1",
+};
+
+export default theme;
